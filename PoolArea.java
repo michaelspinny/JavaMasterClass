efbc3d13542d98de59469a05dc5dab32ef8d35cb
Mykhailo Portianov <73239525+michaelspinny@users.noreply.github.com>
2022-04-24 15:24:48 +0300

    Create PoolArea.java

---
+++ PoolArea.java
@@ -0,0 +1,50 @@
+// Rectangle
+public class Rectangle {
+
+    private double width;
+    private double length;
+
+    public Rectangle(double width, double length) {
+        if (width < 0) {
+            width = 0;
+        }
+        this.width = width;
+        if (length < 0) {
+            length = 0;
+        }
+        this.length = length;
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public double getLength() {
+        return length;
+    }
+    public double getArea() {
+        return length * width;
+    }
+}
+
+// Cuboid
+public class Cuboid extends Rectangle {
+
+    private double height;
+
+    public Cuboid(double width, double length, double height) {
+        super(width, length);
+        if (height < 0) {
+            height = 0;
+        }
+        this.height = height;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public double getVolume() {
+        return height * getArea();
+    }
+}
